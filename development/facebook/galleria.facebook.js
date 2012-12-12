5fd50babbf17e2faf8866eb1682e4c5ddf5b7cab
Nathan Van Gheem <vangheem@gmail.com>
2012-12-12 08:27:22 -0600

    initial

---
+++ development/facebook/galleria.facebook.js
@@ -0,0 +1,39 @@
+(function($) {
+
+var FACEBOOK_URL = 'https://graph.facebook.com';
+
+// keep the original around
+var load = Galleria.prototype.load;
+
+/* extend the load method to use facebook if option is present.
+   this is the way the Galleria flickr plugin */
+Galleria.prototype.load = function() {
+    /* check if facebook option is provided. Call the original load
+       if not. */
+    if(typeof this._options.facebook !== 'string'){
+        load.apply(this, Galleria.utils.array(arguments));
+        return;
+    }
+
+    var self = this,
+        facebook = this._options.facebook;
+
+    $.ajax({
+        url: FACEBOOK_URL + '/' + facebook + '/photos?fields=picture,source&limit=50',
+        dataType: 'json',
+        success: function(data){
+            var images = [];
+            for(var i=0; i<data.data.length; i++){
+                var _image = data.data[i];
+                images.push({
+                    image: _image.source,
+                    thumb: _image.picture
+                });
+            }
+            self._data = images;
+            self.trigger(Galleria.DATA);
+        }
+    });
+};
+
+}(jQuery));
---
+++ development/themes/basic/galleria.basic.js
@@ -0,0 +1,11 @@
+(function($) {
+Galleria.addTheme({
+    name: 'basic',
+    author: 'Galleria How-to',
+    css: 'galleria.basic.css',
+    defaults: { transition: 'fade' },
+    init: function(options) {
+    }
+});
+
+}(jQuery));
